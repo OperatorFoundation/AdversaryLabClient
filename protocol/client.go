2430441d767926d5f5590620357bb64353d018a2
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2019-12-03 14:35:27 -0600

    Refactored database schema for simplification

--- protocol/client.go
+++ protocol/client.go
@@ -9,58 +9,17 @@ import (
 )
 
 const (
-	packetStatsKey = "Packet_Stats"
-
-	allowedConnectionsKey = "Allowed_Connections"
-	allowedIncomingKey    = "Allowed_Incoming_Packets"
-	allowedOutgoingKey    = "Allowed_Outgoing_Packets"
-	allowedIncomingDatesKey = "Allowed_Incoming_Dates"
-	allowedOutgoingDatesKey = "Allowed_Outgoing_Dates"
-
-	blockedConnectionsKey = "Blocked_Connections"
-	blockedIncomingKey    = "Blocked_Incoming_Packets"
-	blockedOutgoingKey    = "Blocked_Outgoing_Packets"
-	blockedIncomingDatesKey = "Blocked_Incoming_Dates"
-	blockedOutgoingDatesKey = "Blocked_Outgoing_Dates"
-
-	rawPacketStatsKey = "RawPacket_Stats"
-
-	allowedRawConnectionsKey = "Allowed_RawConnections"
-	allowedIncomingNetworkPacketKey = "Allowed_Incoming_Network_Packets"
-	allowedOutgoingNetworkPacketKey = "Allowed_Outgoing_Network_Packets"
-	allowedIncomingTransportPacketKey = "Allowed_Incoming_Transport_Packets"
-	allowedOutgoingTransportPacketKey = "Allowed_Outgoing_Transport_Packets"
-	allowedRawIncomingDatesKey = "Allowed_Incoming_Dates"
-	allowedRawOutgoingDatesKey = "Allowed_Outgoing_Dates"
-
-	blockedRawConnectionsKey = "Blocked_RawConnections"
-	blockedIncomingNetworkPacketKey = "Blocked_Incoming_Network_Packets"
-	blockedOutgoingNetworkPacketKey = "Blocked_Outgoing_Network_Packets"
-	blockedIncomingTransportPacketKey = "Blocked_Incoming_Transport_Packets"
-	blockedOutgoingTransportPacketKey = "Blocked_Outgoing_Transport_Packets"
-	blockedRawIncomingDatesKey = "Blocked_Incoming_Dates"
-	blockedRawOutgoingDatesKey = "Blocked_Outgoing_Dates"
-
-	allowedPacketsSeenKey = "Allowed_Connections_Seen"
-	blockedPacketsSeenKey = "Blocked_Connections_Seen"
-
-	pubsubChannel = "New_Connections_Channel"
-	pubsubMessage = "NewConnectionAdded"
+	packetsKey = "Packets"
 )
-
-type Connection struct {
-	Id    string `gorethink:"id,omitempty"`
-	IncomingPayload  []byte `gorethink:"incoming_packet"`
-	IncomingTimestamp int64 `gorethink:"incoming_timestamp"`
-	OutgoingPayload  []byte `gorethink:"outgoing_packet,omitempty"`
-	OutgoingTimestamp int64 `gorethink:"outgoing_timestamp,omitempty"`
-}
 
 type RawPacket struct {
 	Connection    string `gorethink:"connection"`
 	IPPacket  []byte `gorethink:"ip_packet"`
 	TCPPacket  []byte `gorethink:"tcp_packet"`
+	Payload []byte `gorethink:"payload"`
 	Timestamp int64 `gorethink:"timestamp"`
+	AllowBlock bool `gorethink:"allow_block"`
+	InOut bool `gorethink:"in_out"`
 }
 
 // Client holds the connection to the Redis database
@@ -125,19 +84,7 @@ func makeConnectionID() string {
 func (client Client) AddRawTrainPacket(allowBlock bool, conn RawConnectionPackets) {
 	connectionIDString := makeConnectionID()
 
-	_, tableCreateErr := rethinkdb.DB("AdversaryLab").TableCreate(allowedIncomingNetworkPacketKey).RunWrite(client.session)
-	if tableCreateErr != nil {
-		fmt.Println("Could not create table", tableCreateErr)
-	}
-	_, tableCreateErr = rethinkdb.DB("AdversaryLab").TableCreate(allowedOutgoingNetworkPacketKey).RunWrite(client.session)
-	if tableCreateErr != nil {
-		fmt.Println("Could not create table", tableCreateErr)
-	}
-	_, tableCreateErr = rethinkdb.DB("AdversaryLab").TableCreate(blockedIncomingNetworkPacketKey).RunWrite(client.session)
-	if tableCreateErr != nil {
-		fmt.Println("Could not create table", tableCreateErr)
-	}
-	_, tableCreateErr = rethinkdb.DB("AdversaryLab").TableCreate(blockedOutgoingNetworkPacketKey).RunWrite(client.session)
+	_, tableCreateErr := rethinkdb.DB("AdversaryLab").TableCreate(packetsKey).RunWrite(client.session)
 	if tableCreateErr != nil {
 		fmt.Println("Could not create table", tableCreateErr)
 	}
@@ -146,6 +93,7 @@ func (client Client) AddRawTrainPacket(allowBlock bool, conn RawConnectionPacket
 
 		var incomingNetworkLayerContents []byte
 		var incomingTransportLayerContents []byte
+		var incomingPayload []byte
 		var incomingTime = incomingPacket.Metadata().CaptureInfo.Timestamp.UnixNano() / 1000000
 
 		fmt.Println(connectionIDString)
@@ -161,24 +109,28 @@ func (client Client) AddRawTrainPacket(allowBlock bool, conn RawConnectionPacket
 			incomingTransportLayerContents = incomingTransportLayer.LayerContents()
 		}
 
+		if iapp := incomingPacket.ApplicationLayer(); iapp != nil {
+			incomingPayload = iapp.Payload()
+		}
+
 		connValue := RawPacket{
 			Connection: connectionIDString,
 			IPPacket:   incomingNetworkLayerContents,
 			TCPPacket:  incomingTransportLayerContents,
+			Payload: incomingPayload,
 			Timestamp:  incomingTime,
+			AllowBlock: allowBlock,
+			InOut: true,
 		}
 
-		if allowBlock {
-			rethinkdb.DB("AdversaryLab").Table(allowedIncomingNetworkPacketKey).Insert(connValue).RunWrite(client.session)
-		} else {
-			rethinkdb.DB("AdversaryLab").Table(blockedIncomingNetworkPacketKey).Insert(connValue).RunWrite(client.session)
-		}
+		rethinkdb.DB("AdversaryLab").Table(packetsKey).Insert(connValue).RunWrite(client.session)
 	}
 
 	// If  there is an outgoing packet, be sure to save that packet and its timestamp too.
 	for  _, outgoingPacket := range conn.Outgoing {
 		var outgoingNetworkLayerContents []byte
 		var outgoingTransportLayerContents []byte
+		var outgoingPayload []byte
 		var outgoingTime = outgoingPacket.Metadata().CaptureInfo.Timestamp.UnixNano() / 1000000
 
 		fmt.Println(outgoingNetworkLayerContents)
@@ -193,50 +145,67 @@ func (client Client) AddRawTrainPacket(allowBlock bool, conn RawConnectionPacket
 			outgoingTransportLayerContents = outgoingTransportLayer.LayerContents()
 		}
 
+		if oapp := outgoingPacket.ApplicationLayer(); oapp != nil {
+			outgoingPayload = oapp.Payload()
+		}
+
 		connValue := RawPacket{
 			Connection: connectionIDString,
 			IPPacket:   outgoingNetworkLayerContents,
 			TCPPacket:  outgoingTransportLayerContents,
+			Payload: outgoingPayload,
 			Timestamp:  outgoingTime,
+			AllowBlock: allowBlock,
+			InOut: false,
 		}
 
-		if allowBlock {
-			rethinkdb.DB("AdversaryLab").Table(allowedOutgoingNetworkPacketKey).Insert(connValue).RunWrite(client.session)
-		} else {
-			rethinkdb.DB("AdversaryLab").Table(blockedOutgoingNetworkPacketKey).Insert(connValue).RunWrite(client.session)
-		}
+		rethinkdb.DB("AdversaryLab").Table(packetsKey).Insert(connValue).RunWrite(client.session)
 	}
 }
 
 // AddTrainPacket adds a packet to the training data set
 func (client Client) AddTrainPacket(allowBlock bool, conn ConnectionPackets) {
-	var connValue Connection
+	var connValue RawPacket
 
 	connectionIDString := makeConnectionID()
 	incomingPacket := conn.Incoming
 
+	var incomingNetworkLayerContents []byte
+	var incomingTransportLayerContents []byte
 	var incomingPayload []byte
 	var incomingTime = incomingPacket.Metadata().CaptureInfo.Timestamp.UnixNano() / 1000000
 
 	fmt.Println(incomingPayload)
 	fmt.Println(incomingTime)
 
+	if incomingNetworkLayer := incomingPacket.NetworkLayer(); incomingNetworkLayer != nil {
+		incomingNetworkLayerContents = incomingNetworkLayer.LayerContents()
+	}
+
+	if incomingTransportLayer := incomingPacket.TransportLayer(); incomingTransportLayer != nil {
+		incomingTransportLayerContents = incomingTransportLayer.LayerContents()
+	}
+
 	if iapp := incomingPacket.ApplicationLayer(); iapp != nil {
 		incomingPayload = iapp.Payload()
 	}
 
-	var key string
-
-	if allowBlock {
-		key = allowedConnectionsKey
-	} else {
-		key = blockedConnectionsKey
-	}
-
-	_, tableCreateErr := rethinkdb.DB("AdversaryLab").TableCreate(key).RunWrite(client.session)
+	_, tableCreateErr := rethinkdb.DB("AdversaryLab").TableCreate(packetsKey).RunWrite(client.session)
 	if tableCreateErr != nil {
 		fmt.Println("Could not create table", tableCreateErr)
 	}
+
+	connValue = RawPacket{
+		Connection: connectionIDString,
+		IPPacket:   incomingNetworkLayerContents,
+		TCPPacket:  incomingTransportLayerContents,
+		Payload:    incomingPayload,
+		Timestamp:  incomingTime,
+		AllowBlock: allowBlock,
+		InOut:      true,
+	}
+
+	rethinkdb.DB("AdversaryLab").Table(packetsKey).Insert(connValue).RunWrite(client.session)
 
 	// In some cases we will get a conn that only has an incoming packet
 	// This should only ever happen if the conn is blocked
@@ -244,20 +213,36 @@ func (client Client) AddTrainPacket(allowBlock bool, conn ConnectionPackets) {
 	if  conn.Outgoing != nil {
 		outgoingPacket := conn.Outgoing
 
+		var outgoingNetworkLayerContents []byte
+		var outgoingTransportLayerContents []byte
 		var outgoingPayload []byte
 		var outgoingTime = outgoingPacket.Metadata().CaptureInfo.Timestamp.UnixNano() / 1000000
 
 		fmt.Println(outgoingPayload)
 		fmt.Println(outgoingTime)
 
+		if outgoingNetworkLayer := outgoingPacket.NetworkLayer(); outgoingNetworkLayer != nil {
+			outgoingNetworkLayerContents = outgoingNetworkLayer.LayerContents()
+		}
+
+		if outgoingTransportLayer := outgoingPacket.TransportLayer(); outgoingTransportLayer != nil {
+			outgoingTransportLayerContents = outgoingTransportLayer.LayerContents()
+		}
+
 		if oapp := outgoingPacket.ApplicationLayer(); oapp != nil {
 			outgoingPayload = oapp.Payload()
 		}
 
-		connValue = Connection{Id: connectionIDString, IncomingPayload: incomingPayload, IncomingTimestamp: incomingTime, OutgoingPayload: outgoingPayload, OutgoingTimestamp: outgoingTime}
-	} else {
-		connValue = Connection{Id: connectionIDString, IncomingPayload: incomingPayload, IncomingTimestamp: incomingTime, OutgoingPayload: nil, OutgoingTimestamp: 0}
-	}
+		connValue = RawPacket{
+			Connection: connectionIDString,
+			IPPacket:   outgoingNetworkLayerContents,
+			TCPPacket:  outgoingTransportLayerContents,
+			Payload:    outgoingPayload,
+			Timestamp:  outgoingTime,
+			AllowBlock: allowBlock,
+			InOut:      false,
+		}
 
-	rethinkdb.DB("AdversaryLab").Table(key).Insert(connValue).RunWrite(client.session)
+		rethinkdb.DB("AdversaryLab").Table(packetsKey).Insert(connValue).RunWrite(client.session)
+	}
 }
